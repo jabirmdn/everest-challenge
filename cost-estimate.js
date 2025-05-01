04b1fbc72bd1bd6e4d9070dac1cbab2e4ed06d06
jabirmdn <jabir.mdn@gmail.com>
2025-05-01 08:43:01 +0530

    create separate entry files for the problems

---
+++ cost-estimate.js
@@ -0,0 +1,26 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+let baseDeliveryCost = 0;
+let numberOfPackages = 0;
+let packagesRead = 0;
+
+let lineCount = 0;
+
+rl.on('line', (line) => {
+	if (lineCount === 0) {
+		const [cost, count] = line.trim().split(' ').map(Number);
+		baseDeliveryCost = cost;
+		numberOfPackages = count;
+		lineCount++;
+	} else {
+		const [id, weight, distance, offerCode] = line.trim().split(' ');
+    packagesRead++
+		if (packagesRead === numberOfPackages) {
+		}
+	}
+});
